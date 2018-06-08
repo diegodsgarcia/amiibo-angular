e71446116d0e798db7643d1a8d3d89726ac3c11c
diegodsgarcia <diego.ds.garcia@gmail.com>
2018-06-08 10:15:43 -0300

    Create model amiibo

---
+++ src/models/Amiibo.ts
@@ -0,0 +1,13 @@
+
+export class Amiibo {
+  constructor(
+    public name: string,
+    public character: string,
+    public amiiboSeries: string,
+    public gameSeries: string,
+    public image: string,
+    public release: object[]
+  ) {
+
+  }
+}
